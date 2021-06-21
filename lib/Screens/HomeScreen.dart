7bd6077f46e0f7843f6ac5f5d85d29d5dc2b531f
snehanimkarde <snehanimkarde@gmail.com>
2021-06-21 14:57:32 +0530

    [200~MyTravel_App~

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mytravel_app/widgets/Destination_Carousel.dart';
+import 'package:mytravel_app/widgets/Hotel_Carousel.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  int _currentTab = 0;
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 28.0),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 12, left: 12),
+              child: Text(
+                "What you would like \n to find ?",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: _icons
+                    .asMap()
+                    .entries
+                    .map(
+                      (MapEntry map) => _buildIcon(map.key),
+                    )
+                    .toList()),
+            SizedBox(
+              height: 20,
+            ),
+            DestinationCarousel(),
+            SizedBox(
+              height: 20,
+            ),
+            HoteCarousel(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentTab,
+          onTap: (int value) {
+            setState(() {
+              _currentTab = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search, size: 30.0), title: SizedBox.shrink()),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.local_pizza, size: 30.0), title: Text('')),
+            BottomNavigationBarItem(
+                icon: CircleAvatar(
+                  radius: 15.0,
+                  backgroundImage:
+                      NetworkImage('http://i.imgur.com/zL4Krbz.jpg'),
+                ),
+                title: Text(''))
+          ]),
+    );
+  }
+}
